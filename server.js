32089527512271291ff905c5f1866f3add811394
Mlungisi Nzimande <mrfeather01.@gmail.com>
2021-12-26 16:22:08 +0200

    file upload

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
+const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
 const colors = require('colors');
+const fileupload = require('express-fileupload');
 const errorHandler = require('./middleware/error');
 
 //Load env vars
@@ -24,6 +26,12 @@ if (process.env.NODE_ENV === 'development') {
 
 //Body parser
 app.use(express.json());
+
+//File uploading
+app.use(fileupload());
+
+//Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 //Mount routers
 app.use('/api/v1/bootcamps', bootcamps);
